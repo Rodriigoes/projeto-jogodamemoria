d5809aae4596854ba42ff57dd83d636b05a17598
Rodrigo Silva <rodrigopsilva.profissional@gmail.com>
2023-03-13 15:25:14 -0300

    salvo

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front"
-const CARD = "card_back"
+const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
 let techs = [
     'bootstrap',
@@ -20,6 +22,47 @@ startGame();
 function startGame() {
     cards = creatCardsFromTechs(techs);
     shuffleCards(cards);
+
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+    let GameBoard = document.getElementById("gameBoard");
+
+    cards.forEach(card => {
+
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList .add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard)
+        GameBoard.appendChild(cardElement);
+    })
+
+}
+
+function createCardContent(card, cardElement) {
+    creatCardFace(FRONT, card, cardElement);
+    creatCardFace(BACK, card, cardElement);
+}
+
+function creatCardFace(face, card, element) {
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT) {
+        let icoElement = document.createElement('img');
+        iconElement.classList.add(ICON)
+        iconElement.src = "./assets//images" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards) {
@@ -36,9 +79,9 @@ function shuffleCards(cards) {
  
 function creatCardsFromTechs(techs) {
     let cards = [];
-    for(let tech of techs){
+    techs.forEach((tech) => {
         cards.push(createPairFromTech(tech));
-    }
+    })
 
     return cards.flatMap(pair => pair);
 }
@@ -57,4 +100,8 @@ function creatPairFroTech(tech) {
 
 function createIdWithTech(tech){
     return tech + parseInt(Math.random() *1000);
+}
+
+function flipCard() {
+
 }
