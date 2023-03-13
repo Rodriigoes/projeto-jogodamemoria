eeddf2e105a90e04728556742167e54cd844ba6f
Rodrigo Silva <rodrigopsilva.profissional@gmail.com>
2023-03-13 15:39:16 -0300

    .

--- scripts/script.js
+++ scripts/script.js
@@ -3,105 +3,84 @@ const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron'
-    'firebase',
-    'html',
-    'javascript',
-    'jquery'
-    'mongo',
-    'node',
-    'react'];
-
-let cards = null;
-
 startGame();
 
 function startGame() {
-    cards = creatCardsFromTechs(techs);
-    shuffleCards(cards);
-
-    initializeCards(cards);
+    initializeCards(game.createCardsFromTechs());
 }
 
 function initializeCards(cards) {
-    let GameBoard = document.getElementById("gameBoard");
-
-    cards.forEach(card => {
+    let gameBoard = document.getElementById("gameBoard");
+    gameBoard.innerHTML = '';
+    game.cards.forEach(card => {
 
         let cardElement = document.createElement('div');
         cardElement.id = card.id;
-        cardElement.classList .add(CARD);
+        cardElement.classList.add(CARD);
         cardElement.dataset.icon = card.icon;
 
         createCardContent(card, cardElement);
 
         cardElement.addEventListener('click', flipCard)
-        GameBoard.appendChild(cardElement);
+        gameBoard.appendChild(cardElement);
     })
-
 }
 
 function createCardContent(card, cardElement) {
-    creatCardFace(FRONT, card, cardElement);
-    creatCardFace(BACK, card, cardElement);
+
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+
+
 }
 
-function creatCardFace(face, card, element) {
+function createCardFace(face, card, element) {
 
     let cardElementFace = document.createElement('div');
     cardElementFace.classList.add(face);
-    if(face === FRONT) {
-        let icoElement = document.createElement('img');
-        iconElement.classList.add(ICON)
-        iconElement.src = "./assets//images" + card.icon + ".png";
+    if (face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/images/" + card.icon + ".png";
         cardElementFace.appendChild(iconElement);
-
     } else {
         cardElementFace.innerHTML = "&lt/&gt";
     }
     element.appendChild(cardElementFace);
 }
 
-function shuffleCards(cards) {
-    let currentIndex = cards.lenght;
-    let randomIndex = 0;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
-    }
-}
- 
-function creatCardsFromTechs(techs) {
-    let cards = [];
-    techs.forEach((tech) => {
-        cards.push(createPairFromTech(tech));
-    })
-
-    return cards.flatMap(pair => pair);
-}
-
-function creatPairFroTech(tech) {
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false
-    },{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false
-    }]
-}
-
-function createIdWithTech(tech){
-    return tech + parseInt(Math.random() *1000);
-}
 
 function flipCard() {
 
+    if (game.setCard(this.id)) {
+
+        this.classList.add("flip");
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+                if (game.checkGameOver()) {
+                    let gameOverLayer = document.getElementById("gameOver");
+                    gameOverLayer.style.display = 'flex';
+                }
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+
+            };
+        }
+    }
+
+}
+
+function restart() {
+    game.clearCards();
+    startGame();
+    let gameOverLayer = document.getElementById("gameOver");
+    gameOverLayer.style.display = 'none';
 }
